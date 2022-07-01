33c18d15be92082e5edbb4900c8b5294a6d03769
GitHub <noreply@github.com>
2022-07-01 00:17:09 +0000

    :arrow_up: Update Dependencies

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 import { GatewayIntents } from "./deps.ts";
-import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.146.0/http/server.ts";
 import Pekusara from "./bot.ts";
 
 let token = Deno.env.get("token");
