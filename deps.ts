104f75e4883056ee29b9fb406abf0908b76745ab
GitHub <noreply@github.com>
2022-07-13 00:17:45 +0000

    :arrow_up: Update Dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/x/harmony@v2.6.0/mod.ts";
 export * from "https://deno.land/x/xkcd@0.1.0/mod.ts";
-export { default as joke } from "https://deno.land/x/joke@v1.0.0/mod.ts";
+export { default as joke } from "https://deno.land/x/joke@v2.0.0/mod.ts";
